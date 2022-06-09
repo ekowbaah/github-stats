d71e65d54ebac6d2d0582e5c92fafe583db3c4db
Ekow Baah-Nyarkoh <baahekow1@gmail.com>
2022-06-09 18:46:11 +0200

    fix: add netlify redirects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  client_id: 'd31daf692001dd08152b',
-  client_secret: '01c5ffb81caa9252c8fd70b5e13f322b9e57ca66',
+  client_id: '358da723c0500a80ef16',
+  client_secret: 'a83a095f953fe99d670bc8ca545bc67a279020c1',
   oauthBaseUrl: 'login/oauth',
   redirect_uri: 'https://gilded-eclair-1d99c1.netlify.app/auth/callback',
 };
