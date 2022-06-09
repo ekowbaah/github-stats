bce495c1f9a76ff69f0f0a4b36921bb261a49ed0
Ekow Baah-Nyarkoh <baahekow1@gmail.com>
2022-06-09 19:41:43 +0200

    fix: add netlify redirects

--- src/app/auth/services/auth-service.service.ts
+++ src/app/auth/services/auth-service.service.ts
@@ -24,13 +24,11 @@ export class AuthService {
     let headers = new HttpHeaders({
       Accept: 'application/json',
     });
-    let r = (Math.random() + 1).toString(36).substring(7);
     const queryParams = {
       client_id: environment.client_id,
       client_secret: environment.client_secret,
       code,
       redirect_uri: environment.redirect_uri,
-      state: this.getRandomString(),
     };
     return this.http
       .post(
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
+export const environment: any = {
   production: true,
   client_id: '358da723c0500a80ef16',
   client_secret: 'a83a095f953fe99d670bc8ca545bc67a279020c1',
