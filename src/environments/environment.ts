4718d98943a6b8c2b60ea050836e1e03f39978f1
Ekow Baah-Nyarkoh <baahekow1@gmail.com>
2022-06-12 11:43:50 +0200

    update client secret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment: any = {
   production: false,
   client_id: 'dc7c4ac7128a5d2fdc4c',
-  client_secret: '415770e78ac371d5ffa0f6dabb1dc5c4840dbbd7',
+  client_secret: 'b4e437ad55b00523581d33a9e7ea1927875ad30f',
   oauthBaseUrl: 'login/oauth',
   redirect_uri: `${window.location.origin}/auth/callback`,
 };
+
+
