1307f0a59b50a302dcc6871b0839f82102966025
Ekow Baah-Nyarkoh <39923127+ekowbaah@users.noreply.github.com>
2022-09-09 11:25:21 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment: any = {
   production: false,
-  client_id: 'dc7c4ac7128a5d2fdc4c',
-  client_secret: 'b4e437ad55b00523581d33a9e7ea1927875ad30f',
+  client_id: '',
+  client_secret: '',
   oauthBaseUrl: 'login/oauth',
   redirect_uri: `${window.location.origin}/auth/callback`,
 };
