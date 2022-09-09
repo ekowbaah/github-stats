63b311495b18fcd6b56d505269a6da80352a9794
Ekow Baah-Nyarkoh <39923127+ekowbaah@users.noreply.github.com>
2022-09-09 11:25:40 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment: any = {
   production: true,
-  client_id: '358da723c0500a80ef16',
-  client_secret: 'cdc594779775143bea5f38fa643d5fe7479add0b',
+  client_id: '',
+  client_secret: '',
   oauthBaseUrl: 'login/oauth',
   redirect_uri: 'https://gilded-eclair-1d99c1.netlify.app/auth/callback',
 };
